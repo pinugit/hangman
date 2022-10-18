9f1692fbd7c0dc326b7f178e1702e108e114719b
pinu <pinuwork@gmail.com>
2022-10-18 10:59:07 +0530

    deleted hangmanpics file and unwanted code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 def wordinit():
     #this function will select a random word from the file words.txt 
@@ -13,19 +12,3 @@ def wordinit():
     random_selected_word = random.choice(words_list)
     
     return random_selected_word
-
-
-def printuserword(word, letter):
-    #This function will print out the word in "_" format and if the letter is present which is given it will replace "_" with the letter
-    for letters in word:
-        if letter is letters:
-            sys.stdout.write(letter)
-        else :
-            sys.stdout.write("_")
-
-# guesses = 6
-
-# # while guesses > 0:
-# #     user_guess = chr(input("Enter your guess :"))
-    
-# #     if 
